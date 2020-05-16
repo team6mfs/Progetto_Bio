9ad61c543e9a02b5901f96f523603b3738c357db
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-05-16 12:01:16 +0200

    Add project files.

---
+++ BioTest/BioTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace BioTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int a = int.Parse(Console.ReadLine());
+        }
+    }
+}
+ 
---
+++ LibreriaBio/Bio.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LibreriaBio
+{
+    public class Bio
+    {
+    }
+}
